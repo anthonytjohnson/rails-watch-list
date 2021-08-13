c829b7d731da5e01916ece2e77ccfedd89e0d7f2
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-08-13 12:20:45 +0900

    fixed card, photo not loading

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   resources :lists, only: [:index, :new, :show, :create] do
     resources :bookmarks, only: [ :new, :create]
   end
-  resources :bookmarks, only: [:destroy]
   resources :movies, only: [] do
     resources :bookmarks, only: [:new, :create]
   end
